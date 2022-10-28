9aca2cab2153ebc281fcd5eef076b5841564e076
Martina Lacková <martinalackovab@gmail.com>
2022-10-28 17:50:38 +0900

    Movement_Collision

---
+++ Assets/Scenes/collisionScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class collisionScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision obstacleCollision) //information about collision, obstackeCollisio is the name of that information
+    {
+        Debug.Log("hittttt");
+
+        if (obstacleCollision.collider.tag == "Obstacle") //information.collider (which collider we hit).tag
+        {
+            Debug.Log("hit");
+        }
+    }
+}
